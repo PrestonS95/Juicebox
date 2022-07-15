8059c36d33c85ee932da9d9013b6848252cc52a5
PrestonS95 <prestonsullins95@gmail.com>
2022-07-15 10:03:40 -0500

    Added more functions to index.js

--- db/seed.js
+++ db/seed.js
@@ -15,5 +15,39 @@ async function testDB() {
     client.end();
   }
 }
+async function dropTables() {
+  try {
+    await client.query(`
 
-testDB();
+    `);
+  } catch (error) {
+    throw error; // we pass the error up to the function that calls dropTables
+  }
+}
+
+// this function should call a query which creates all tables for our database
+async function createTables() {
+  try {
+    await client.query(`
+
+    `);
+  } catch (error) {
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+rebuildDB();
+
